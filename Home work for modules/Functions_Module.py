bddca4a69704dcde8a8b165da2c265b777dd1a3a
Slavaway <101943893+Slavaway@users.noreply.github.com>
2022-10-10 19:53:26 +0600

    Add files via upload
    Здравствуйте, когда вы будете запускать модуль, пожалуйста впишите значения, которые она попросит и программа продолжится, я пока что не знаю почему этот блок когда выводит первее чем меню.

---
+++ Home work for modules/Functions_Module.py
@@ -0,0 +1,87 @@
+def task1():
+    The_First_Number = input("Введите первое число a: ")
+    The_Second_Number = input("Введите второе число b: ")
+    The_Third_Number = input("Введите третье число: ")
+
+    if The_First_Number > The_Second_Number:
+        print(The_Second_Number)
+
+    elif The_Second_Number > The_First_Number:
+        print(The_First_Number)
+
+    elif The_Third_Number > The_First_Number:
+        print(The_First_Number)
+    elif The_Third_Number > The_Second_Number:
+        print(The_Second_Number)
+
+    elif The_First_Number > The_Third_Number:
+        print(The_Third_Number)
+
+    else:
+        print(The_Third_Number)
+
+def task2():
+    print("Введите коэффициенты уравнения типа: ax^2+bx+c")
+
+
+a_var = int(input("a: "))
+b_var = int(input("b: "))
+c_var = int(input("c: "))
+
+
+D = (b_var* b_var) - 4 * a_var * c_var
+
+if D > 0:
+    print("ДА")
+
+elif D == 0:
+    print("ДА")
+
+else:
+    quit()
+
+def task3():
+    City_Caption = input()
+    City_List =  City_Caption.split(' ')
+
+    if 'Moscow' not in City_List: 
+        City_List.append('Moscow')
+    print(City_List)
+    
+
+
+def task4():
+    a_sec = input("Введите время в секундах: ")
+
+    b_sec = input("Введите время в минутах: ")
+
+    if a_sec > b_sec:
+        print(a_sec)
+
+    else:
+        print(b_sec) 
+
+        
+
+def task5():
+    while True:
+        print("1. Кафедра ТК:")
+        print("2. Факультет ФИСТ")
+        print("3. Каферда Радиотехника")
+        print("0. Выйти")
+        User_Inpt = input("Выберите пункт: ")
+
+        if User_Inpt == "1":
+            print("Выбран 1 пункт")
+        elif User_Inpt == "2":
+            print("Выбран 3 пункт")
+        elif User_Inpt == "3":
+            print("Выбран 3 пункт")
+        elif User_Inpt == "0":
+            break
+        else:
+            print("Вы ввели не правильное значение")
+
+
+
+
---
+++ Home work for modules/Module for tasks.py
@@ -0,0 +1,40 @@
+import Functions_Module
+
+while True: 
+    print("1.      task1")
+    print("2.      task2 ")
+    print("3.      task3")
+    print("4.      task4 ")
+    print("5.      task5")
+    User_Input_The_Inf = input("Введите  вашу задачу: ")
+
+    if User_Input_The_Inf == "1":
+        Functions_Module.task1()
+
+    elif User_Input_The_Inf == "2":
+        Functions_Module.task2()
+    elif User_Input_The_Inf == "3":
+        Functions_Module.task3()
+    elif User_Input_The_Inf == "4":
+        Functions_Module.task4()
+    elif User_Input_The_Inf == "5":
+        Functions_Module.task5()
+
+    else: 
+        print("Error, you will be redirected to the main manu...")
+
+        
+    
+
+
+
+
+    
+    
+
+
+
+
+
+
+
